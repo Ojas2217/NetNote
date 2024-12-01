29183302c27049ac69c6b70ec9d9ec507c86f341
Vilius Birgeris <v.birgeris@student.tudelft.nl>
2024-12-01 13:37:44 +0100

    Added NoteDTO to avoid json parsing errors with Pair<>

---
+++ commons/src/main/java/commons/NoteDTO.java
@@ -0,0 +1,30 @@
+package commons;
+
+/**
+ * Note data transfer object for transferring the ID and title.
+ * */
+public class NoteDTO {
+    private Long id;
+    private String title;
+
+    public NoteDTO() {
+        // for object mapper.
+    }
+
+    public NoteDTO(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static NoteDTO of(Long id, String title) {
+        return new NoteDTO(id, title);
+    }
+}
