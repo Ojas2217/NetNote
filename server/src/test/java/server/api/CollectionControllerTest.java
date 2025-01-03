3b2255c27814f4df8740d0736ed211097bca5565
Otte Hamoen <o.h.hamoen@student.tudelft.nl>
2025-01-03 20:19:35 +0100

    changed the name from the class to be more fitting

--- server/src/test/java/server/api/CollectionControllerTest.java
+++ server/src/test/java/server/api/CollectionControllerTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class CollectionServiceTest {
+class CollectionControllerTest {
     private CollectionRepository collectionRepository;
     private CollectionService collectionService;
 
