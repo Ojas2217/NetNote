f8ea54643c138c348c5d2d5ae9572ac49db6adc1
Otte Hamoen <o.h.hamoen@student.tudelft.nl>
2025-01-25 22:46:03 +0100

    added 32 more lines and more branch-coverage for the FileTest class.

--- commons/src/test/java/commons/FileTest.java
+++ commons/src/test/java/commons/FileTest.java
@@ -36,4 +36,48 @@ class FileTest {
         file.setId(33L);
         assertEquals(33L, file.getId());
     }
+
+    @Test
+    void setIdBoundaryValues() {
+        file.setId(0L);
+        assertEquals(0L, file.getId(), "The ID should be set to 0.");
+
+        file.setId(Long.MAX_VALUE);
+        assertEquals(Long.MAX_VALUE, file.getId(), "The ID should be set to Long.MAX_VALUE.");
+    }
+
+    @Test
+    void getNoteWhenNull() {
+        File noNoteFile = new File("noNoteFile", null);
+
+        assertNull(noNoteFile.getNote(), "The note should be null.");
+    }
+
+    @Test
+    void fileNameIsNull() {
+        File nullNameFile = new File(2L, null, note);
+
+        assertNull(nullNameFile.getName(), "The file name should be null.");
+    }
+
+    @Test
+    void fileNameIsEmpty() {
+        File emptyNameFile = new File(3L, "", note);
+
+        assertEquals("", emptyNameFile.getName(), "The file name should be an empty string.");
+    }
+
+    @Test
+    void noteIsNull() {
+        File nullNoteFile = new File(4L, "fileWithoutNote", null);
+
+        assertNull(nullNoteFile.getNote(), "The note should be null when not set.");
+    }
+
+    @Test
+    void fileNameWithSpecialCharacters() {
+        File specialCharFile = new File(5L, "file_@#%$!.txt", note);
+
+        assertEquals("file_@#%$!.txt", specialCharFile.getName(), "The file name should include special characters.");
+    }
 }
