aa683e4a2691d3d060eb32fdf2e19fe56f2562eb
Otte Hamoen <o.h.hamoen@student.tudelft.nl>
2025-01-25 22:39:29 +0100

    added 7 more lines and more branch-coverage for the CollectionTest class.

--- commons/src/test/java/commons/CollectionTest.java
+++ commons/src/test/java/commons/CollectionTest.java
@@ -40,4 +40,13 @@ class CollectionTest {
         String expected = ToStringBuilder.reflectionToString(collection, MULTI_LINE_STYLE);
         assertEquals(expected, collection.toString());
     }
+
+    @Test
+    void setNotes() {
+        List<Note> newNotes = List.of(new Note("new title 1", "new content 1"), new Note("new title 2", "new content 2"));
+        collection.setNotes(newNotes);
+
+        assertEquals(newNotes, collection.getNotes(), "The notes in the collection should be updated to the new list");
+        assertEquals(2, collection.getNotes().size(), "The collection should now contain two notes");
+    }
 }
